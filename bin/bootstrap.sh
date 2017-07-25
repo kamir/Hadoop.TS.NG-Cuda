9b8592b22e19be7933921794f164f45fe2f85204
Mirko Kaempf <mirko.kaempf@cloudera.com>
2017-07-25 21:09:29 +0000

    execution of demo1 via mvn works also in CDSW

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,5 +1,6 @@
 #
 # install external libraries into the local mvn-repo
 #-------------------------------------------------------------------------------
-mvn install:install-file -Durl=file:///home/cdsw/demo1/lib -Dfile=/home/cdsw/demo1/lib/hadoop-ts-core-1.2.4.jar -DgroupId=com.cloudera -DartifactId=hadoop-ts-core -Dpackaging=jar -Dversion=1.2.4
+mvn install:install-file -Durl=file:///home/cdsw/demo1/cuda-tsa/lib -Dfile=/home/cdsw/demo1/cuda-tsa/lib/hadoop-ts-core-1.2.4.jar -DgroupId=com.cloudera -DartifactId=hadoop-ts-core -Dpackaging=jar -Dversion=1.2.4
 
+mvn compile exec:java -Dexec.mainClass="connectors.cmtsq.ClouderaManageTSQClient"
