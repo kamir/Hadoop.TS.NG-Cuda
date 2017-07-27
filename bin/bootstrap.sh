091448fa26facda52505d201282a79dd14c2cc3c
Mirko <mirko.kaempf@gmail.com>
2017-07-27 17:07:53 +0200

    Prepare for merge with working solution from CDSW

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,5 +1,5 @@
 #
-# install external libraries into the local mvn-repo
+# install external libraries into the local Maven-repo
 #-------------------------------------------------------------------------------
 mvn install:install-file -Durl=file:///home/cdsw/demo1/cuda-tsa/lib -Dfile=/home/cdsw/demo1/cuda-tsa/lib/hadoop-ts-core-1.2.4.jar -DgroupId=com.cloudera -DartifactId=hadoop-ts-core -Dpackaging=jar -Dversion=1.2.4
 
@@ -8,3 +8,4 @@ mvn compile exec:java -Dexec.mainClass="connectors.cmtsq.ClouderaManageTSQClient
 mvn compile exec:java -Dexec.mainClass="cudatsa.rng.RNGExperiments"
 
 mvn compile exec:java -Dexec.mainClass="tswb.tsa.generator.ExampleBucketCreator"
+
