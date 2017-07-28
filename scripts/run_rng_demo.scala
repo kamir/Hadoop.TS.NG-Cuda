323950811275c108402d77db625faafc1e6cf6d3
Mirko Kaempf <mirko.kaempf@cloudera.com>
2017-07-28 16:56:43 +0000

    prep for merge with clean project

--- scripts/run_rng_demo.scala
+++ scripts/run_rng_demo.scala
@@ -1,7 +1,12 @@
+import jcurand.samples.JCurandSample
+val args = Array("1")
+JCurandSample.main( args )
+
+
+  
+  
 import cudatsa.rng.RNGExperiments
-
-// 10 rounds
+// 1 round
 // no chart, only console output
-val args = Array("1", "false", "false")
-
+val args = Array("1", "false", "true")
 RNGExperiments.main( args )
