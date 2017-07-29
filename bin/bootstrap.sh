b6893a0b0b83a71d27319674e987bd5e19f3d053
Mirko <mirko.kaempf@gmail.com>
2017-07-29 17:02:59 +0200

    Added deployment step for CDSW session to bootstrap script

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -7,7 +7,11 @@ mvn install:install-file -Durl=file:///home/cdsw/demo1/cuda-tsa/lib -Dfile=/home
 
 mvn compile exec:java -Dexec.mainClass="connectors.cmtsq.ClouderaManageTSQClient"
 
-mvn compile exec:java -Dexec.mainClass="cudatsa.rng.RNGExperiments"
+mvn clean package
 
-mvn compile exec:java -Dexec.mainClass="tswb.tsa.generator.ExampleBucketCreator"
+cp target/cuda-tsa-0.2.0-SNAPSHOT-jar-with-dependencies.jar lib/all2.jar
+
+#mvn compile exec:java -Dexec.mainClass="cudatsa.rng.RNGExperiments"
+
+#mvn compile exec:java -Dexec.mainClass="tswb.tsa.generator.ExampleBucketCreator"
 
